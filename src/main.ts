1a4417c549dc7d979da9437e31c990b7755f807b
Katherine Yang <whykatherine@gmail.com>
2023-02-14 15:57:29 -0500

    feat: make title a little interactive element

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
 import "./style.css";
+
+import { ProsePlay } from "./proseplay/proseplay";
+
+(new ProsePlay(document.querySelector(".title") as HTMLElement)).parseText("(Prose|Puzzle)(Play|Poetry)");
--- src/playground.ts
+++ src/playground.ts
@@ -2,11 +2,17 @@ import "./style.css";
 
 import { ProsePlay } from "./proseplay/proseplay";
 
+(new ProsePlay(document.querySelector(".title") as HTMLElement)).parseText("(Prose|Puzzle)(Play|Poetry)");
+
 const container = document.querySelector(".text") as HTMLElement;
 const pp = new ProsePlay(container);
 const input = document.querySelector("#input") as HTMLTextAreaElement,
   submit = document.querySelector("#submit") as HTMLElement;
+input.value = `this is (my|your|our)
+very (lovely|cool|weird)
+(poem|experience)`;
 submit.addEventListener("click", e => {
   e.preventDefault();
   pp.parseText(input.value);
 });
+submit.click();
