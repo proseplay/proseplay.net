1517d81e464d57055ddab588a3bfafcea901b365
Katherine Yang <whykatherine@gmail.com>
2023-02-13 22:33:43 -0500

    clean out shader files

--- src/main2.ts
+++
@@ -1,7 +0,0 @@
-import "./style2.css";
-
-import { ProsePlay } from "./proseplay";
-
-const textEl = document.querySelector(".text") as HTMLElement;
-const pp = new ProsePlay(textEl);
-pp.loadSample("carpenter");
