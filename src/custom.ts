29e95fc777410f53786d722a642d561a1f136254
Katherine Yang <whykatherine@gmail.com>
2023-02-14 01:34:07 -0500

    content

--- src/custom.ts
+++ src/custom.ts
@@ -5,6 +5,12 @@ import { ProsePlay } from "./proseplay/proseplay";
 const container = document.querySelector(".text") as HTMLElement;
 const pp = new ProsePlay(container);
 const text = `
+in the mo( |u)rning
+
+
+(to those who are dreamt—  
+do you know what you are?)
+
 do you know that I dream about you?
 you with (your strange dress|your familiar laugh)
 you with (a face I couldn’t pick from a lineup|your body that my body remembers sitting near)
@@ -13,12 +19,16 @@ you with (a face I couldn’t pick from a lineup|your body that my body remember
 what wakes in my s(l| )eep
 so s( |l)eeps when day breaks
 you with your dress and
-that face you
-so traipse and leave
-but a trace or seed
+that face in the chaff
 
-your name slips off my tongue like silk
-you crumble by my eyes like silt
-oh, but I grieve, to leave my love( |s) in a dream 
+you traipse and leave
+but a trace or seed
+in the mess of light and
+feathers too hard by half
+
+their names slip off my tongue like sil(k|t)
+they all crumble by my eyes as sil(t|k)
+oh, but I grieve
+to (leave|love) my (love|leave) in a dream 
 `;
 pp.parseText(text);
