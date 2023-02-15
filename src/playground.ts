6a484b757ec65de901f2bda60e051161ca6378bd
Katherine Yang <whykatherine@gmail.com>
2023-02-15 13:56:24 -0500

    feat: default demos linking

--- src/playground.ts
+++ src/playground.ts
@@ -8,8 +8,8 @@ const container = document.querySelector(".text") as HTMLElement;
 const pp = new ProsePlay(container);
 const input = document.querySelector("#input") as HTMLTextAreaElement,
   submit = document.querySelector("#submit") as HTMLElement;
-input.value = `this is (my|your|our)
-very (lovely|cool|weird)
+input.value = `this is (my|your|our)[1]
+very (lovely|cool|weird)[1]
 (poem|experience)`;
 submit.addEventListener("click", e => {
   e.preventDefault();
