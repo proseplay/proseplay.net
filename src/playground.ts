85a7230a31c4bd16645d8a0f005dc6e35e7c6b61
Katherine Yang <whykatherine@gmail.com>
2023-04-11 14:45:11 -0400

    focus mode

--- src/playground.ts
+++ src/playground.ts
@@ -3,94 +3,147 @@ import "./style.css";
 import { ProsePlay } from "proseplay";
 
 const title = document.querySelector(".title") as HTMLElement;
-(new ProsePlay(title)).parse(title.innerText);
 
-const container = document.querySelector(".text") as HTMLElement;
-const pp = new ProsePlay(container);
-const input = document.querySelector("#input") as HTMLTextAreaElement,
-  submit = document.querySelector("#submitBtn") as HTMLButtonElement,
-  randomise = document.querySelector("#randomiseBtn") as HTMLButtonElement,
-  expand = document.querySelector("#expandBtn") as HTMLButtonElement,
-  snapshot = document.querySelector("#snapshotBtn") as HTMLButtonElement;
+const container = document.querySelector(".text") as HTMLElement,
+  input = document.querySelector("#input") as HTMLTextAreaElement,
+  submitBtn = document.querySelector("#submitBtn") as HTMLButtonElement;
 
-const snapshotContainer = document.querySelector(".snapshots") as HTMLElement,
+const randomiseBtn = document.querySelector("#randomiseBtn") as HTMLButtonElement,
+  expandBtn = document.querySelector("#expandBtn") as HTMLButtonElement;
+
+const focusBtn = document.querySelector("#focusBtn") as HTMLButtonElement,
+  focusNote = document.querySelector(".focus-note") as HTMLElement;
+  
+const switcher = document.querySelector(".switcher") as HTMLElement;
+const viewer = document.querySelector(".viewer") as HTMLElement;
+  
+const snapshotBtn = document.querySelector("#snapshotBtn") as HTMLButtonElement,
+  snapshotContainer = document.querySelector(".snapshots") as HTMLElement,
   snapshotTemplate = document.querySelector(".snapshot") as HTMLElement;
 snapshotTemplate.remove();
 let snapshots: HTMLElement[] = [];
 
-const loadBtns = document.querySelectorAll(".loadBtn") as NodeListOf<HTMLButtonElement>;
-loadBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    fetch(`/samples/${btn.value}.txt`)
-      .then(r => r.text())
-      .then(text => {
-        input.value = text;
-      });
+const ppTitle = new ProsePlay(title),
+  pp = new ProsePlay(container),
+  ppSwitcher = new ProsePlay(switcher);
+
+document.addEventListener("DOMContentLoaded", () => {
+  ppTitle.parse(title.innerText);
+
+  ppSwitcher.parse(switcher.innerText);
+  ppSwitcher.setFunction("viewInput", viewInput);
+  ppSwitcher.setFunction("viewOutput", viewOutput);
+
+  const loadBtns = document.querySelectorAll(".loadBtn") as NodeListOf<HTMLButtonElement>;
+  loadBtns.forEach(btn => {
+    btn.addEventListener("click", load);
   });
-})
+  
+  input.value = `in the (mist|missed) (see|sea)
+  (prey|pray) in the (morning|mourning)
+  for (words|worlds) that (exit|exist)
+  as (seep|sleep)`;
+  
+  submitBtn.addEventListener("click", submit);
+  
+  randomiseBtn.addEventListener("click", () => pp.randomise());
+  
+  expandBtn.addEventListener("click", toggleExpand);
+  
+  snapshotBtn.addEventListener("click", snapshot);
+  
+  focusBtn.addEventListener("click", focus);
+  
+  window.addEventListener("keydown", e => {
+    if (e.key === "Escape" && document.body.classList.contains("focus")) {
+      unfocus();
+    }
+  });
+});
 
-input.value = `in the (mist|missed) (see|sea)
-(prey|pray) in the (morning|mourning)
-for (words|worlds) that (exit|exist)
-as (seep|sleep)`;
+function load(e: Event) {
+  const btn = e.currentTarget as HTMLButtonElement;
+  fetch(`/samples/${btn.value}.txt`)
+    .then(r => r.text())
+    .then(text => {
+      input.value = text;
+    });
+}
 
-const ppSwitcher = new ProsePlay(document.querySelector(".switcher") as HTMLElement);
-ppSwitcher.parse((document.querySelector(".switcher") as HTMLElement).innerText);
-ppSwitcher.setFunction("viewInput", viewInput);
-ppSwitcher.setFunction("viewOutput", viewOutput);
-
-submit.addEventListener("click", e => {
-  e.preventDefault();
+function submit(e?: Event) {
+  e?.preventDefault();
   pp.parse(input.value);
   ppSwitcher.slideWindow(0, 1);
   viewOutput();
 
-  expand.disabled = false;
-  randomise.disabled = false;
-  snapshot.disabled = false;
+  expandBtn.disabled = false;
+  randomiseBtn.disabled = false;
+  snapshotBtn.disabled = false;
+  focusBtn.disabled = false;
 
   snapshots.forEach(snapshot => snapshot.remove());
   snapshots = [];
-});
+}
 
-randomise.addEventListener("click", () => pp.randomise());
-
-expand.addEventListener("click", () => {
+function toggleExpand() {
   pp.isExpanded ? pp.collapse() : pp.expand();
-  expand.innerText = pp.isExpanded ? "Collapse" : "Expand";
-  randomise.disabled = pp.isExpanded;
-});
+  expandBtn.innerText = pp.isExpanded ? "Collapse" : "Expand";
+  randomiseBtn.disabled = pp.isExpanded;
+}
 
-snapshot.addEventListener("click", () => {
-  let snapshotHtml = snapshotTemplate.cloneNode(true) as HTMLElement;
+function snapshot() {
+  const snapshotHtml = snapshotTemplate.cloneNode(true) as HTMLElement;
   snapshotContainer.appendChild(snapshotHtml);
   snapshots.push(snapshotHtml);
 
-  let snapshotHeading = snapshotHtml.querySelector(".snapshot--heading") as HTMLElement;
+  const snapshotHeading = snapshotHtml.querySelector(".snapshot--heading") as HTMLElement;
   snapshotHeading.innerText = `Snapshot ${snapshots.length}`;
 
-  let snapshotText = snapshotHtml.querySelector(".snapshot--text") as HTMLElement;
-  let text = pp.snapshot();
+  const snapshotText = snapshotHtml.querySelector(".snapshot--text") as HTMLElement;
+  const text = pp.snapshot();
   snapshotText.innerText = text;
 
-  let snapshotClear = snapshotHtml.querySelector(".snapshot--clear") as HTMLElement;
+  const snapshotClear = snapshotHtml.querySelector(".snapshot--clear") as HTMLElement;
   snapshotClear.addEventListener("click", () => {
     snapshotHtml.remove();
-    let index = snapshots.indexOf(snapshotHtml);
+    const index = snapshots.indexOf(snapshotHtml);
     snapshots.splice(index, 1);
     snapshots.forEach((otherSnap, i) => {
       (otherSnap.querySelector(".snapshot--heading") as HTMLElement).innerText = `Snapshot ${i + 1}`;
     });
   });
-});
+}
+
+function focus() {
+  document.body.classList.add("focus");
+  setTimeout(() => {
+    document.querySelectorAll(".hidable").forEach(el => el.classList.add("invisible"));
+  }, 500);
+
+  focusNote.classList.remove("invisible");
+  focusNote.style.opacity = "1";
+  setTimeout(() => {
+    setTimeout(() => {
+      focusNote.style.opacity = "0";
+      setTimeout(() => {
+        focusNote.classList.add("invisible");
+      }, 500);
+    }, 1000);
+  }, 1000);
+}
+
+function unfocus() {
+  document.body.classList.remove("focus");
+  document.querySelectorAll(".hidable").forEach(el => el.classList.remove("invisible"));
+}
 
 function viewInput() {
-  (document.querySelector(".viewer") as HTMLElement).classList.add("view-input");
-  (document.querySelector(".viewer") as HTMLElement).classList.remove("view-output");
+  viewer.classList.add("view-input");
+  viewer.classList.remove("view-output");
 }
 
 function viewOutput() {
-  submit.click();
-  (document.querySelector(".viewer") as HTMLElement).classList.add("view-output");
-  (document.querySelector(".viewer") as HTMLElement).classList.remove("view-input");
+  submitBtn.click();
+  viewer.classList.add("view-output");
+  viewer.classList.remove("view-input");
 }
