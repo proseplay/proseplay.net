f9bc13fe09109044a8195bd8be2d0d0264075f5e
Katherine Yang <whykatherine@gmail.com>
2025-03-09 15:23:23 -0400

    add keyboard shortcuts to display page

--- src/display.ts
+++ src/display.ts
@@ -2,16 +2,41 @@ import { ProsePlay } from "proseplay";
 import { getHash, getTextFromHash } from "./common";
 
 window.addEventListener("DOMContentLoaded", () => init());
+window.addEventListener("keydown", handleKeydown);
 
 const textArea = document.querySelector(".text") as HTMLElement;
 const sourceAnchor = document.querySelector("#source-anchor") as HTMLAnchorElement;
 
+const pp: ProsePlay = new ProsePlay(textArea);
+
 function init() {
   const text = getTextFromHash();
-  const pp = new ProsePlay(textArea);
   pp.parse(text);
 
   const hash = getHash();
   const sourceLink = `/playground/#${hash}`;
   sourceAnchor.href = sourceLink; 
+}
+
+function handleKeydown(e: KeyboardEvent) {
+  switch (e.key) {
+    case "r": {
+      e.preventDefault();
+      randomise();
+      break;
+    }
+    case "e": {
+      e.preventDefault();
+      toggleExpand();
+      break;
+    }
+  }
+}
+
+function randomise() {
+  pp.randomise();
+}
+
+function toggleExpand() {
+  pp.isExpanded ? pp.collapse() : pp.expand();
 }
