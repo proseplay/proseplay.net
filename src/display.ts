3bcd85e30852cbcbdc88e84d00350a0383694b71
Katherine Yang <whykatherine@gmail.com>
2025-03-10 08:31:11 -0400

    add buttons to display page

--- src/display.ts
+++ src/display.ts
@@ -5,9 +5,17 @@ window.addEventListener("DOMContentLoaded", () => init());
 window.addEventListener("keydown", handleKeydown);
 
 const textArea = document.querySelector(".text") as HTMLElement;
+const pp: ProsePlay = new ProsePlay(textArea);
+
 const sourceAnchor = document.querySelector("#source-anchor") as HTMLAnchorElement;
 
-const pp: ProsePlay = new ProsePlay(textArea);
+const detailBtn = document.querySelector("#detailBtn") as HTMLButtonElement;
+const randomiseBtn = document.querySelector("#randomiseBtn") as HTMLButtonElement;
+
+randomiseBtn.addEventListener("click", () => pp.randomise());
+randomiseBtn.addEventListener("pointerdown", () => randomiseBtn.classList.add("clicked"));
+randomiseBtn.addEventListener("pointerup", () => randomiseBtn.classList.remove("clicked"));
+detailBtn.addEventListener("click", toggleExpand);
 
 function init() {
   const text = getTextFromHash();
@@ -39,4 +47,8 @@ function randomise() {
 
 function toggleExpand() {
   pp.isExpanded ? pp.collapse() : pp.expand();
+  (detailBtn.querySelector("span") as HTMLElement).innerText = pp.isExpanded ? "Collapse" : "Expand";
+  detailBtn.classList.toggle("expand");
+  detailBtn.classList.toggle("collapse");
+  randomiseBtn.disabled = pp.isExpanded;
 }
